395ae2b97ca5863c2b6155dcc7a9326448c733b8
xpoorkids <mike.calverts@outlook.com>
2026-02-01 22:46:17 -0600

    Add run_scan function with stubbed data

---
+++ app/services/scanner.py
@@ -0,0 +1,10 @@
+def run_scan():
+    # V1: stubbed data
+    return [
+        {
+            "symbol": "TEST",
+            "velocity": 320,
+            "volume_delta": 2.1,
+            "liquidity": 180000
+        }
+    ]
